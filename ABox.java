9ea4f801d2edcedfc3b3c3c0025867b69c9d9f3f
David Jourdan <dav.jourdan@gmail.com>
2015-12-16 11:37:55 +0100

    initialisation

---
+++ ABox.java
@@ -0,0 +1,16 @@
+
+public class ABox extends MBox 
+{
+	
+	public ABox(int x, int y, Maze m) {
+		super(x, y, m);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getLabel()
+	{		
+		return "A";
+		
+	}
+
+}
