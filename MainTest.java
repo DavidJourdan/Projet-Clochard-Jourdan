ffa476705e9c4800380fcb17fb777e3b4e128f9f
David Jourdan <dav.jourdan@gmail.com>
2015-12-17 11:21:31 +0100

    Modification des arguments

--- MainTest.java
+++ MainTest.java
@@ -10,7 +10,7 @@ public class MainTest {
 	 */
 	public static void main(String[] args) throws MazeReadingException, IOException {
 		
-		Maze maze = new Maze("data/labyrinthe.txt", 10, 10);
+		Maze maze = new Maze(10, 10);
 		maze.initFromTextFile("data/labyrinthe.txt");
 		maze.saveToTextFile("data/labyrinthe2.txt");
 	}
