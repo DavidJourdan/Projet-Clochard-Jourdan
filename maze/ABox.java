c68c04266e02e88444850b2e4b6b8bd1e9fe7ce8
David <dav.jourdan@gmail.com>
2015-12-24 16:27:23 +0100

    correction faute de frappe

--- maze/ABox.java
+++ maze/ABox.java
@@ -1,17 +1,17 @@
 package maze;
 
-public class WBox extends MBox
+public class ABox extends MBox
 {
     public Maze m;
 
-    public WBox(int x, int y, Maze m) {
+    public ABox(int x, int y, Maze m) {
         super(x, y, m);
         // TODO Auto-generated constructor stub
     }
 
     public String getLabel()
     {
-        return "W";
+        return "A";
 
     }
 
