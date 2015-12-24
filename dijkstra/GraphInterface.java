3b5a93e70b58e5b50662c1efd63614cfcce5e4f5
David <dav.jourdan@gmail.com>
2015-12-24 14:58:32 +0100

    ajout package dijkstra

---
+++ dijkstra/GraphInterface.java
@@ -0,0 +1,9 @@
+package dijkstra;
+
+import java.util.ArrayList;
+
+public interface GraphInterface {
+    public ArrayList<VertexInterface> getAllVertices();
+    public ArrayList<VertexInterface> getSuccessors(VertexInterface vertex);
+    public int getWeight(VertexInterface src, VertexInterface dst);
+}
