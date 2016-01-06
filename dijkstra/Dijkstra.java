b6b79afd1dc68608e180820cda42cb39db947223
David Jourdan <dav.jourdan@gmail.com>
2016-01-06 19:45:27 +0100

    resolution d'un conflit pour le merge (reverted from commit dcb40826c4c4a247efb33077214885348f7da25c)

--- dijkstra/Dijkstra.java
+++ dijkstra/Dijkstra.java
@@ -3,6 +3,6 @@ package dijkstra;
 public class Dijkstra {
     private static PreviousInterface dijkstra(ASetInterface a, GraphInterface g, PiInterface pi,
                                               PreviousInterface previous, VertexInterface r) {
-
+        return null;
     }
 }
